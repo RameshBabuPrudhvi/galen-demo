ea82e0f2abde94f74429f3d39595a01fb2c42819
Ramesh Babu Prudhvi <rameshbabu.p@yahoo.com>
2024-08-13 22:07:31 +0530

    Fixed Specs

--- src/test/java/org/example/tests/DashboardTest.java
+++ src/test/java/org/example/tests/DashboardTest.java
@@ -7,13 +7,15 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.testng.Assert.assertEquals;
+
 public class DashboardTest extends TestFixer {
 
     @DataProvider(name = "layoutTestData")
     public Object[][] layoutTestData() {
         return new Object[][]{
-                {"http://localhost:4200/#/dashboard", "src/test/resources/specs/dashboard-page-expected.gspec", "Dashboard layout test"},
-                {"http://localhost:4200/#/table-list-a", "src/test/resources/specs/table-page-expected.gspec", "Table layout test"}
+                {"http://localhost:4200/#/dashboard-b", "src/test/resources/specs/dashboard-page-expected.gspec", "Dashboard layout test"},
+                {"http://localhost:4200/#/table-list-b", "src/test/resources/specs/table-page-expected.gspec", "Table layout test"}
         };
     }
 
@@ -28,5 +30,13 @@ public class DashboardTest extends TestFixer {
         var test = GalenTestInfo.fromString(testName);
         test.getReport().layout(layoutReport, testName);
         testInfo.add(test);
+        if (layoutReport.errors() > 0) {
+            var errorMessage = new StringBuilder("Layout errors found: ");
+            errorMessage.append(layoutReport.errors()).append("\n");
+            layoutReport.getValidationErrorResults().forEach(error ->
+                    errorMessage.append(error.getError().getMessages()).append("\n")
+            );
+            assertEquals(layoutReport.errors(), 0, errorMessage.toString());
+        }
     }
 }
