8f95af055169bba21819bd787da8d9f2f67b81ba
Ramesh Babu Prudhvi <rameshbabu.p@yahoo.com>
2024-08-14 17:12:24 +0530

    Added logo validation Specs

--- src/test/java/org/example/tests/DashboardTest.java
+++ src/test/java/org/example/tests/DashboardTest.java
@@ -13,9 +13,14 @@ public class DashboardTest extends TestFixer {
 
     @DataProvider(name = "layoutTestData")
     public Object[][] layoutTestData() {
+
+        String DASHBOARD_URL = "http://localhost:4200/#/dashboard";
+        String TABLE_URL = "http://localhost:4200/#/table-list-a";
+
         return new Object[][]{
-                {"http://localhost:4200/#/dashboard-b", "src/test/resources/specs/dashboard-page-expected.gspec", "Dashboard layout test"},
-                {"http://localhost:4200/#/table-list-b", "src/test/resources/specs/table-page-expected.gspec", "Table layout test"}
+                {DASHBOARD_URL, "src/test/resources/specs/dashboard-page-expected.gspec", "Dashboard layout test"},
+                {DASHBOARD_URL, "src/test/resources/specs/logo.gspec", "Image Comparison Test"},
+                {TABLE_URL, "src/test/resources/specs/table-page-expected.gspec", "Table layout test"}
         };
     }
 
@@ -23,13 +28,15 @@ public class DashboardTest extends TestFixer {
     public void testPageLayout(String url, String specPath, String testName) throws Exception {
         // Navigate to the URL
         driver.get(url);
-
         // Run the layout check and create a report
         var layoutReport = Galen.checkLayout(driver, specPath, List.of("desktop"));
 
         var test = GalenTestInfo.fromString(testName);
+
         test.getReport().layout(layoutReport, testName);
+
         testInfo.add(test);
+
         if (layoutReport.errors() > 0) {
             var errorMessage = new StringBuilder("Layout errors found: ");
             errorMessage.append(layoutReport.errors()).append("\n");
