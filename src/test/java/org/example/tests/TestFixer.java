2e6e38d9769f49162632144e805601ebad78cf19
Ramesh Babu Prudhvi <rameshbabu.p@yahoo.com>
2024-08-17 17:51:02 +0530

    Added multi browser support

--- src/test/java/org/example/tests/TestFixer.java
+++ src/test/java/org/example/tests/TestFixer.java
@@ -4,8 +4,10 @@ import com.galenframework.reports.GalenTestInfo;
 import com.galenframework.reports.HtmlReportBuilder;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,14 +19,19 @@ public class TestFixer {
     boolean isCurrentEnvironment = false;
     List<GalenTestInfo> testInfo = new ArrayList<>();
 
+    @Parameters("browser")
     @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+    public void setUp(String browser) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
         driver.manage().window().maximize();
         //driver.manage().window().setSize(new Dimension(1200, 800));
     }
-
 
     @AfterClass
     public void tearDown() throws IOException {
@@ -34,4 +41,5 @@ public class TestFixer {
         }
         new HtmlReportBuilder().build(testInfo, REPORT_PATH);
     }
+
 }
