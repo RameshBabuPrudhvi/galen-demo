aa95e12eb094b5e512a0693740ad9e9ec6e04c69
Ramesh Babu Prudhvi <rameshbabu.p@yahoo.com>
2025-02-04 14:15:00 +0530

    Cleanup

--- src/test/java/org/example/tests/SendMoneyPageTest.java
+++ src/test/java/org/example/tests/SendMoneyPageTest.java
@@ -11,30 +11,45 @@ import org.testng.annotations.Test;
 
 public class SendMoneyPageTest extends TestFixer {
     public static final String URL = "https://www.westernunion.com/in/en/web/send-money/start";
+    public static final String COUNTRY = "United States";//"Australia"
 
     @DataProvider(name = "layoutTestData")
     public Object[][] layoutTestData() {
 
         return new Object[][]{
-                {"sendMoney.gspec", "Send Money Page Dimensions Validation Test", "demoCheck"},
-                {"logo.gspec", "Logo Test", "imgCheck"}
+                {"font.gspec", "Font Validation", "fontCheck"},
+                {"color.gspec", "Color Validation", "colorCheck"},
+                {"aligned.gspec", "Alignment Validation", "alignmentCheck"}
         };
     }
 
-    @Test(dataProvider = "layoutTestData")
+   // @Test(dataProvider = "layoutTestData")
     public void testPageLayout(ITestContext context, String specFile, String testName, String groupName) {
         String specPath = "src/test/resources/specs/wu/";
         GalenHelper.verifyPageLayout(context, specPath + specFile, testName, groupName);
     }
 
+
+    @Test
+    public void testPageLayout1(ITestContext context) {
+        sendMoney();
+        String specPath = "src/test/resources/specs/wu/sendMoney.gspec";
+        GalenHelper.verifyPageLayout(context, specPath, "Send Money Page Dimensions Validation Test", "positionCheck");
+    }
+
     @BeforeTest
-    public void login() {
+    public void beforeTest() {
         var driver = DriverManager.getDriver();
         driver.get(URL);
-        var wait = new WebDriverWait(driver, 30);
+        var wait = new WebDriverWait(driver, 10);
         wait.until(d -> d.findElement(By.id("btn_lead_registration_cancel")).isDisplayed());
         driver.findElement(By.id("btn_lead_registration_cancel")).click();
-        driver.findElement(By.xpath("//li[normalize-space()='United States']")).click();
+    }
+
+    public void sendMoney() {
+        var driver = DriverManager.getDriver();
+        var wait = new WebDriverWait(driver, 30);
+        driver.findElement(By.xpath("//li[normalize-space()='" + COUNTRY + "']")).click();
         driver.findElement(By.id("btn_country_selection_continue")).click();
         wait.until(driver1 -> driver1.findElement(By.id("label_estimate_details_selected_country")).isDisplayed());
         driver.findElement(By.id("input-estimate_details_sender_field")).sendKeys("1000");
